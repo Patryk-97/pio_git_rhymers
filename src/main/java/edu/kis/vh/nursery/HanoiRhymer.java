54c9f3694204f6a59060ebdcec9ae0e513da2f04
Patryk Stepien <215886@edu.p.lodz.pl>
2020-01-16 11:12:05 +0100

    1.3. Bledy formatowania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,21 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+
+//Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+//5, 12, 14, 15.
+
+//Skróty alt + -> oraz alt + <- służą do przemieszczania się po ostatnio zmodyfikowanych liniach kodu projektu
